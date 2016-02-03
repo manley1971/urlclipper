ba57518f0f58de09cea8d5d572f60da631a5f0a9
Dave Manley <manley@stanfordalumni.org>
2016-02-03 06:56:47 +0000

    better error handling--found a crash

--- server.js
+++ server.js
@@ -13,25 +13,33 @@ require('dotenv').load();
 var url = 'mongodb://localhost:27017/';
 app.use('/public', express.static(process.cwd() + '/public'));
 
+//Is this a url to add to our collection?
 app.get('/new/https?\://:url', function(req, res) {
 //    console.log(req);
     let retval = {};
     let s = req.params.url;
     retval.id = shortid;
     shortid=shortid+"a";
- //   retval.original_url = "https:\/\/" + s;
     retval.original_url = req.url.split("new/")[1];
     mongo.connect(url, function(err, db) {
-        var p = db.collection('urls');
+      if (!db) {
+          console.log("********no db**********");
+	  res.end("mongo is not running");
+       }
+      else {
+       var p = db.collection('urls');
         p.insert(retval, function(err, data) {
             // handle error
             if (err) console.log("Error in db insert.");
             // other operations
             db.close();
         });
+     }
     });
     res.end(JSON.stringify(retval));
 })
+
+// or is this obviously poorly formed
 app.get('/new/*', function(req, res) {
     let retval = {
         "error": "URL invalid"
@@ -39,14 +47,21 @@ app.get('/new/*', function(req, res) {
     res.send(JSON.stringify(retval));
 });
 
+//the user is presented a key for which they wish to be redirected
 app.get('/:shortner', function(req, res) {
     mongo.connect(url, function(err, db) {
-        var p = db.collection('urls');
-        p.find({
-            "id": req.params.shortner
-        }, {
+        if (!db) {
+          console.log("no db");
+	  res.end("mongo is not running");
+       }
+
+       else {
+         var p = db.collection('urls');
+         p.find({
+              "id": req.params.shortner
+          }, {
             "original_url": 1
-        }).toArray(function(err, docs) {
+          }).toArray(function(err, docs) {
             if (docs && docs[0]) {
                 let redirect_url = docs[0].original_url;
                 console.log('sel:' + JSON.stringify(redirect_url));
@@ -62,7 +77,9 @@ app.get('/:shortner', function(req, res) {
             }
             db.close();
             res.end();
+        
         });
+      }
     });
 });
 
