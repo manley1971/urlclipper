8c3263cc8b29fd07167c060a26682bdcdbc1c2e5
Dave Manley <manley@stanfordalumni.org>
2016-02-15 02:35:50 +0000

    change to mongoose

--- server.js
+++ server.js
@@ -1,44 +1,61 @@
 'use strict';
 //the shortid module is not liked by heroku
 //var shortid = require('shortid');
+var mongoose = require('mongoose');
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var session = require('express-session');
-var mongo = require('mongodb');
 var app = express();
-var shortid = "a";
+var shortid = "b";
 require('dotenv').load();
 
-var url = 'mongodb://localhost:27017/';
 app.use('/public', express.static(process.cwd() + '/public'));
+// we use mongoose managed by mongolab 
+console.log("mongo uri:"+process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI);
+
+var SiteSchema = new mongoose.Schema({
+    short_url: String,
+    original_url: String,
+});
+let sModel = mongoose.model('SiteList', SiteSchema);
 
 //Is this a url to add to our collection?
 app.get('/new/https?\://:url', function(req, res) {
-//    console.log(req);
     let retval = {};
-    let s = req.params.url;
-    retval.id = shortid;
-    shortid=shortid+"a";
     retval.original_url = req.url.split("new/")[1];
-    mongo.connect(url, function(err, db) {
-    //if for some reason we did not connect to the db we don't want to crash.
-      if (!db) {
-          console.log("********no db**********");
-	  res.end("mongo is not running");
-       }
-      else {
-       var p = db.collection('urls');
-        p.insert(retval, function(err, data) {
-            // handle error
-            if (err) console.log("Error in db insert.");
-            // other operations
-            db.close();
-        });
-     }
+    console.log(retval.original_url);
+    let q = sModel.find({original_url:retval.original_url}, {
+        _id: 0,
+        __v: 0
     });
-    res.end(JSON.stringify(retval));
-})
+    if (q) 
+      q.exec(function(err, data) {
+        if (err) res.end("probably the sites were about cats anyway, but there was an error looking it up..");
+        if (data&&data.length) {
+          retval.short_id = data;
+          console.log("found:" + JSON.stringify(data));
+          res.end(JSON.stringify(retval));
+        }
+        else {
+          console.log("not found:" + JSON.stringify(data));
+          retval.short_id = shortid;
+          retval.comment="this service is something like a bookmark";
+          res.end(JSON.stringify(retval));
+          let newSearch = new sModel({
+            original_url:retval.original_url,
+            short_url:shortid
+          });
+          newSearch.save();
+          shortid=shortid+"a";
+        }
+      });
+    else 
+      res.end(JSON.stringify("error: query is null, mongo is maybe down"));
+
+});
 
 // or is this obviously poorly formed
 app.get('/new/*', function(req, res) {
@@ -50,41 +67,32 @@ app.get('/new/*', function(req, res) {
 
 //the user is presented a key for which they wish to be redirected
 app.get('/:shortner', function(req, res) {
-    mongo.connect(url, function(err, db) {
-        if (!db) {
-          console.log("no db");
-	  res.end("mongo is not running");
-       }
-
-       else {
-         var p = db.collection('urls');
-         p.find({
-              "id": req.params.shortner
-          }, {
-            "original_url": 1
-          }).toArray(function(err, docs) {
-            if (docs && docs[0]) {
-                let redirect_url = docs[0].original_url;
-                console.log('sel:' + JSON.stringify(redirect_url));
-                res.writeHead(301, {
-                    Location: redirect_url
-                });
-            } else {
-                console.log('Token is not found');
-                let retval = {
-                    "error": "No short url for given input"
-                };
-                res.send(JSON.stringify(retval));
-            }
-            db.close();
-            res.end();
-        
-        });
-      }
+   console.log("requesting shortened site:"+req.params.shortner);
+   let s = req.params.shortner;
+   let q = sModel.find({short_url:s}, {
+        _id: 0,
+        __v: 0
+    });
+    ////submit query that gets everything
+    if (q) 
+      q.exec(function(err, data) {
+        if (err) res.end("probably the sites were about cats anyway, but there was an error looking it up..");
+        if (data) {
+          console.log("found in database, time to relocate to: "+data[0].original_url);
+          res.writeHead(301, {
+                    Location: data[0].original_url
+          });
+          res.end();
+        } else {
+          console.log('Token is not found');
+          let retval = {"error": "No short url for given input"};
+          res.end(JSON.stringify(retval));
+        }
     });
 });
 
 app.use('/', express.static(process.cwd() + '/public'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log('Node.js listening on port ' + port + '...');
